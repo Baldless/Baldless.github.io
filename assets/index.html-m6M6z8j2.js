8a0acc397a730df3800025fd337117cc08f69465
邹璟丞 <shengcunwudi@qq.com>
2023-12-04 13:42:00 +0800

    1

---
+++ assets/index.html-m6M6z8j2.js
@@ -0,0 +1,8 @@
+import{_ as r,o as n,c as t,a as e,b as s}from"./app-Og8nUgVw.js";const a={},o=s('<h1 id="旅行安排" tabindex="-1"><a class="header-anchor" href="#旅行安排" aria-hidden="true">#</a> 旅行安排</h1><h2 id="周五" tabindex="-1"><a class="header-anchor" href="#周五" aria-hidden="true">#</a> 周五</h2><p>从昆山出发，大概开车5小时，到达连云港大概10点，周五酒店定在屿海共眠，然后在附近吃晚饭（夜宵），可以看精力考虑去不去<strong>在海一方</strong>公园，那边全天开放，休息，准备第二天的旅行</p><hr><h2 id="周六-晴" tabindex="-1"><a class="header-anchor" href="#周六-晴" aria-hidden="true">#</a> 周六（晴）</h2><p>早点起来，可以看日出，周六天气好，8到16℃。附近很多吃的，吃早餐。<br><strong>上午</strong>去<strong>海上云台山</strong>，大概游玩3，4个小时。拍照，爬山，看风景。<br><strong>中午</strong>去<strong>老街</strong>逛一下，去那边解决午饭。<br><strong>下午</strong>去<strong>连岛</strong>玩，两个海湾，最后看落日，拍照。<br><strong>晚上</strong>，开车去<strong>墟沟美食城</strong>吃海鲜。住在墟沟美食城附近。</p><h2 id="周日-阴雨" tabindex="-1"><a class="header-anchor" href="#周日-阴雨" aria-hidden="true">#</a> 周日（阴雨）</h2><p><strong>上午</strong>，安排去<strong>花果山游</strong>玩，如果下雨，有精力可以去<strong>渔湾</strong><br> 下午差不多得回昆山</p>',8);function d(i,h){return n(),t("div",null,[e(`
+ * @Description: 
+ * @version: 
+ * @Author: ZJC
+ * @Date: 2023-12-04 08:50:13
+ * @LastEditors: ZJC
+ * @LastEditTime: 2023-12-04 10:35:09
+`),o])}const g=r(a,[["render",d],["__file","index.html.vue"]]);export{g as default};
